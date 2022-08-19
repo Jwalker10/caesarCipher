99ecacf2c7fbf82d82e5a3b4bdb4b0700f6ae6b3
Jwalker10 <62402383+Jwalker10@users.noreply.github.com>
2022-08-19 15:31:57 +0100

    basic boilerplate and creation of Encrypt method, which returns encryptedString

--- caesarCipher.cs
+++ caesarCipher.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+namespace CaesarCipher
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      
+    }
 
+    static string Encrypt(string message) 
+      {
+      // Char array of alphabet
+      char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+      
+      // returns empty string for now to stop errors being thrown!
+      string encryptedString = "";
+      return encryptedString;
+      }
+  }
+}
